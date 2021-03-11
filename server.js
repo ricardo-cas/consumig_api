57b6325cc3bc2aa67a72f98a7e1d94e8fd0af4db
Ricardo Costa <ricardo_cas4@hotmail.com>
2021-03-10 23:19:00 -0300

    criado tratamento de erro no acesso da API pelo front end

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
 
-// ouvindo a API
-app.listen("1010");
-
 // Criando a rota padrão
 app.get("/", (req, res) => {
-  return res.json({ message: "First API!", id: 12093 });
+  return res.json([
+    { name: "Ricardo", id: 12093 },
+    {
+      name: "Indiara",
+      id: 12094,
+    },
+    { name: "Sônia", id: 10934 },
+  ]);
 });
+
+// ouvindo a API
+app.listen("1010");
