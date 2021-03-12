7680edfd1ede3affc2d3b70f8f227c3ee9dca110
Ricardo Costa <ricardo_cas4@hotmail.com>
2021-03-12 19:04:18 -0300

    implementando boas práticas com o try catch para tratamentos de erros

--- server.js
+++ server.js
@@ -8,12 +8,16 @@ app.use(cors());
 
 // Criando a rota padrão
 app.get("/", async (req, res) => {
-  // const response = await axios('https://jsonplaceholder.typicode.com/users') // com o Axios ele já retorna o response com os dados do data ex: response.data
+  try {
+    // const response = await axios('https://jsonplaceholder.typicode.com/users') // com o Axios ele já retorna o response com os dados do data ex: response.data
 
-  // response é a resposta do Axios MAS eu tiro o response do response
-  const { data } = await axios("https://jsonplaceholder.typicode.com/users");
+    // response é a resposta do Axios MAS eu tiro o response do response
+    const { data } = await axios("https://jsonplaceholder.typicode.com/users");
 
-  return res.json(data);
+    return res.json(data);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 // ouvindo a API
