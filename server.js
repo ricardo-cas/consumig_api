6517081b3d7ca04579421a3d991178bc70e27633
Ricardo Costa <ricardo_cas4@hotmail.com>
2021-03-12 18:12:57 -0300

    usando Axios para acessar os dados de uma API no Backend

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const cors = require("cors");
 const express = require("express");
 const app = express();
+const axios = require("axios");
 
 // CORS é um controle de acesso para quando um servidor está conversando com outro
 app.use(cors());
 
 // Criando a rota padrão
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+  // const response = await axios('https://jsonplaceholder.typicode.com/users') // com o Axios ele já retorna o response com os dados do data ex: response.data
+
+  // response é a resposta do Axios MAS eu tiro o response do response
+  const { data } = await axios("https://jsonplaceholder.typicode.com/users");
+  console.log(data);
+
   return res.json([
     { name: "Ricardo", id: 12093 },
     {
